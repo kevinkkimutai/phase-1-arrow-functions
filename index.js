61df5187e80f08203732911676fe4ce74d533ab7
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-22 16:01:02 +0300

    Completed Arrow assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(){
+    const a = 2000
+    const b = 100
+    return a / b
+}
+//arrow function
+let x = 2
+const square = (x) => {
+    return Math.pow(x, 2)
+}
+let a = 3
+let b = 4
+const add = (a, b) => {
+    return a + b
+}
